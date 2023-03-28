cf0d5ca036ad4efa593db66034f6b1f430627801
aronCampos <aron.campos400@gmail.com>
2023-03-28 02:09:05 -0700

    Add html and css to dropdown, still needs works.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import '../App.css';
 
 function Form() {
+    const [bread, setBread] = useState('');
+
     return (
         <div className="Form">
             Form goes here
@@ -11,10 +13,17 @@ function Form() {
                 <button>Add</button>
                 <div class="dropdown">
                     <label>Quantity:</label>
-                    <button class="dropbtn">Dropdown</button>
-                    <div class="dropdown-content">
-                        
+                    <div className='select'>
+                        <span className='selected'>1</span>
+                        <div className='caret'></div>
                     </div>
+                    <ul>
+                        <li>0</li>
+                        <li className='active'>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                        </ul>
                 </div>
 
                 <label>Meat(s)</label>
